5d7b60e92042bd515132df817a31d94406c70d3e
m.rahmani <Ma13R18@>
2023-04-19 10:47:27 +0330

    first commit

---
+++ controllers/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$heading = 'index';
+
+require "views/index.view.php";
---
+++ views/404.php
@@ -0,0 +1,14 @@
+<?php require('partials/head.php') ?>
+<?php require("partials/nav.php") ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold">Sorry. Page not Found.</h1>
+
+            <p>
+                <a href="/" class="text-blue underline">Go back Home.</a>
+            </p>
+        </div>
+    </main>
+
+<?php include("partials/footer.php") ?>
